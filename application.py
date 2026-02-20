324584e69070761af46ed1049b202390d08057c8
Sabrin Adan Hassan <Sabrin.Adan@hotmail.com>
2026-02-20 18:58:53 +0100

    Changed the blueprint

--- application.py
+++ application.py
@@ -1,22 +1,4 @@
-from flask import Flask, render_template
-application = Flask(__name__)
-
-@application.route('/')
-@application.route('/home')
-def home():
-  return render_template('home.html')
-
-@application.route('/methodology')
-def methodology():
-  return render_template('methodology.html', title='methodology')
-
-@application.route('/carbon_app')
-def carbon_app():
-    return render_template('carbon_app.html', title='carbon_app')
-
-@application.route('/aboutUs')
-def aboutUs():
-    return render_template('aboutUs.html', title='aboutUs')
+from capp import application
 
 if __name__ == "__main__":
     application.run(debug=True)
